d48ca63a0913e3f25d6a98e8f5fd1bd2b4fa0df5
Josue N Rivera <josue.n.rivera@outlook.com>
2023-06-15 00:19:14 -0400

    frontend init

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
-import "./App.css";
-import MapViewer from "./Components/MapViewer";
-import { SearchBar } from "./Components/SearchBar";
-import React, { useState, useRef } from "react";
-import buttons from "./Components/buttons.js";
-import HeatmapToggle from "./Components/HeatmapToggle";
-import LogoFootnote from "./Components/LogoFootnote";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [location, setLocation] = useState({
-    longitude: -86.8831443,
-    latitude: 40.41731893,
-    radius: 4250, //meters
-    zoom: 14,
-  });
-  const [LOIResponse, setLOIResponse] = useState(null); //loi = location of interest
-  const locationChangedByInteraction = useRef(false);
-  const [heatmapOn, setHeatmapOn] = useState(false);
-  const [buttonInfo, setButtonInfo] = useState(buttons);
-
   return (
     <div className="App">
-      <MapViewer
-        location={location}
-        locationChangedByInteraction={locationChangedByInteraction}
-        setLocation={setLocation}
-        buttonInfo={buttonInfo}
-        heatmapOn={heatmapOn}
-        LOIResponse={LOIResponse}
-      />
-      <div className="App-header">
-        <div className="Nav-bar">
-          <SearchBar
-            buttonInfo={buttonInfo}
-            setButtonInfo={setButtonInfo}
-            setHeatmapOn={setHeatmapOn}
-            heatmapOn={heatmapOn}
-            location={location}
-            locationChangedByInteraction={locationChangedByInteraction}
-            setLOIResponse={setLOIResponse}
-          />
-          <HeatmapToggle on={heatmapOn} setHeatmapOn={setHeatmapOn} />
-        </div>
-
-        <LogoFootnote />
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
