9038480e2e62185523372da7675f2a77624c7f13
Josue N Rivera <josue.n.rivera@outlook.com>
2023-06-21 15:12:19 -0400

    Create testbed for frontend
    - Provide new logo
    - Initial structure for project
    - Update package dependencies

---
+++ src/Components/MapViewer.js
@@ -0,0 +1,13 @@
+// Textbox.js
+import React, { useState } from 'react';
+import './style.css';
+
+function MapViewer(props) {
+
+  return (
+    <div className="map-viewer">
+    </div>
+  );
+}
+
+export default MapViewer;
