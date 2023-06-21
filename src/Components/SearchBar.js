62736c9c890c7f719f44db88116f541a87627147
Josue N Rivera <josue.n.rivera@outlook.com>
2023-06-21 15:12:19 -0400

    Create testbed for frontend
    - Provide new logo
    - Initial structure for project
    - Update package dependencies

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,94 +1,25 @@
-import React, { useState } from "react";
-import "./style.css";
-import axios from "axios";
-import SearchBox from "./SearchBox";
-import ToggleButton from "./ToggleButton";
-
-function makeSearchTargets(buttonInfo) {
-  let searchTargets = "";
-  for (let i = 0; i < buttonInfo.length; i++) {
-    if (buttonInfo[i].selected) {
-      searchTargets = searchTargets + buttonInfo[i].id + ",";
-    }
-  }
-  return searchTargets.slice(0, -1);
-}
+import React, { useState } from 'react';
+import './style.css';
+import axios from 'axios';
+import SearchBox from './SearchBox';
 
 function SearchBar(props) {
-  const [searchLocation, setSearchLocation] = useState("");
-  const [searchPlaceholder, setSearchPlaceholder] = useState("Lafayette, IN");
-  let searchTargets = makeSearchTargets(props.buttonInfo);
+    const [searchText, setSearchText] = useState("Lafayette, IN, USA");
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    if (!props.heatmapOn) props.setHeatmapOn(true);
-    let searchType = /[\d.]+\W+[\d.]+\W+[\d.]+/.test(searchLocation)
-      ? "locs"
-      : "adds";
-    try {
-      const res = await axios.get(
-        `http://localhost:5000/${searchType}/${searchLocation}/${searchTargets}`
-      );
-      props.setLOIResponse(res.data);
-    } catch (error) {
-      console.error(error);
-      return;
-    }
-    setSearchPlaceholder(searchLocation);
-  };
+    const handleSubmit = async () => {
+        try {
+            const res = await axios.get('http://localhost:5000');
+            props.setLOIResponse(res.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  function changeToggle(key) {
-    let isOnlyOneSelected = true;
-    for (let i = 0; i < props.buttonInfo.length; i++) {
-      if (props.buttonInfo[i].selected && i !== key) {
-        isOnlyOneSelected = false;
-      }
-    }
-    if (isOnlyOneSelected) {
-      alert("You must have at least one LOI selected.");
-    } else {
-      props.setButtonInfo((prevInfo) => {
-        return prevInfo.map((info) => {
-          return info.id === key ? { ...info, selected: !info.selected } : info;
-        });
-      });
-    }
-  }
-
-  const ToggleOptions = props.buttonInfo.map((button) => (
-    <ToggleButton
-      key={button.id}
-      id={button.id}
-      name={button.name}
-      selected={button.selected}
-      changeToggle={changeToggle}
-    />
-  ));
-
-  React.useEffect(() => {
-    if (props.locationChangedByInteraction.current) {
-      let numDecimals = -1 * (11 - props.location.zoom);
-      setSearchLocation(
-        `${props.location.latitude.toFixed(
-          numDecimals
-        )},${props.location.longitude.toFixed(
-          numDecimals
-        )},${props.location.radius.toFixed(0)}`
-      );
-    }
-    props.locationChangedByInteraction.current = false;
-  }, [props.location, props.locationChangedByInteraction, setSearchLocation]);
-
-  return (
-    <form onSubmit={handleSubmit} className="search-bar">
-      <SearchBox
-        searchLocation={searchLocation}
-        setSearchLocation={setSearchLocation}
-        placeholder={searchPlaceholder}
-      />
-      {ToggleOptions}
-    </form>
-  );
+    return (
+        <div className='search-bar'>
+            <SearchBox searchText={searchText} setSearchText={setSearchText} handleSubmit={handleSubmit}/>
+        </div>
+    );
 }
 
-export { SearchBar, makeSearchTargets };
+export default SearchBar;
--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,27 +1,13 @@
-// Textbox.js
-import React, { useState } from 'react';
-import axios from 'axios';
 import './style.css';
 
-function Textbox() {
-  const [response, setResponse] = useState(null);
-
-  const handleClick = async () => {
-    try {
-      const res = await axios.get('<URL>');
-      setResponse(res.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+function SearchBox(props) {
 
   return (
-    <div className="textbox">
+    <div className="searchbox">
       <input type="text" />
-      <button onClick={handleClick}>Submit</button>
-      {response && <div>{JSON.stringify(response)}</div>}
+      <button onClick={props.handleSubmit}>Submit</button>
     </div>
   );
 }
 
-export default Textbox;
+export default SearchBox;
