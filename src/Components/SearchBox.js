65247366da1f04d6a05f2ee985428c01bf017e5b
Josue N Rivera <josue.n.rivera@outlook.com>
2023-06-23 00:19:16 -0400

    Update to searchbox style

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -12,7 +12,7 @@ export default function SearchBox(props) {
         placeholder="Lafayette, IN"
         title="Enter the location of interest"
       />
-      <button type="button" onClick={props.handleSubmit}>Submit</button>
+      <button type="button" onClick={props.handleSubmit}>🔍</button>
     </div>
   );
 }
