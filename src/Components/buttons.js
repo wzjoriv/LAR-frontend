63ef271f6af92e19b5f76b69a8a29ce89675d3c6
PruthviSamirPatel <pruthvipatelcollege@gmail.com>
2023-06-26 15:05:32 -0400

    Included toggle buttons to update search options

---
+++ src/Components/buttons.js
@@ -0,0 +1,25 @@
+const buttons = [
+    {
+        id: 0,
+        name: "Aviation Facilities",
+        selected: false
+    },{
+        id: 1,
+        name: "Fire Stations",
+        selected: false
+    },{
+        id: 2,
+        name: "Hospitals",
+        selected: true
+    },{
+        id: 3,
+        name: "Local Law Enforcement",
+        selected: false
+    },{
+        id: 4,
+        name: "Public Schools",
+        selected: false
+    }
+]
+
+export default buttons
