8e38fa8b37f41f0ef6fec5ea672a37f3c579ca19
Josue N Rivera <josue.n.rivera@outlook.com>
2023-06-27 15:41:04 -0400

    Set map maximum and minimum zoom

--- src/Components/MapViewer.js
+++ src/Components/MapViewer.js
@@ -15,6 +15,7 @@ function MapViewer({ location, LOIResponse, setLocation, isProgrammaticMove }) {
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
+        minZoom: 13,
         attribution:
           '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       }).addTo(mapRef.current);
