75cce062051050d6564d0e6ca2817fda716eebaf
Josue N Rivera <josue.n.rivera@outlook.com>
2023-07-01 01:56:44 -0400

    Detect zoom for view + heatmap worker init

--- src/Components/MapViewer.js
+++ src/Components/MapViewer.js
@@ -1,11 +1,48 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 import L from 'leaflet';
+import axios from "axios";
 import './style.css';
+import renderHeatmap from './heatmap.worker.js';
 
 function MapViewer({ location, LOIResponse, setLocation, isProgrammaticMove }) {
   const mapRef = useRef(null);
+  const locationChangedByMoveEnd = useRef(false);
+
+  const getLocationData = useCallback(async (event) => {
+    console.log(location);
+    try {
+      const res = await axios.get(
+        `http://localhost:5000/locs/${location.latitude},${location.longitude},${location.radius}/1,2`
+      );
+
+      return res.data;
+    } catch (error) {
+      console.error(error);
+
+      return null;
+    }
+  }, [location]);
+
+  const handleMoveEnd = useCallback(() => {
+    if (isProgrammaticMove.current) {
+      isProgrammaticMove.current = false;
+      return;
+    }
+  
+    let bounds = mapRef.current.getBounds();
+    let center = mapRef.current.getCenter();
+  
+    locationChangedByMoveEnd.current = true;
+    setLocation({
+      longitude: center.lng,
+      latitude: center.lat,
+      radius: (center.distanceTo(bounds.getNorthWest())) / 2 + 1000, //meters
+      zoom: mapRef.current.getZoom(),
+    });
+  }, [setLocation, isProgrammaticMove]);
 
   useEffect(() => {
+
     if (!mapRef.current) {
       isProgrammaticMove.current = true;
       mapRef.current = L.map('map-viewer').setView(
@@ -20,33 +57,17 @@ function MapViewer({ location, LOIResponse, setLocation, isProgrammaticMove }) {
           '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       }).addTo(mapRef.current);
 
-      mapRef.current.on('moveend', function () {
-        if (isProgrammaticMove.current) {
-          isProgrammaticMove.current = false;
-          return;
-        }
-
-        console.log(mapRef.current.getCenter().toString());
-
-        // Get radius + execute API call
-
-        setLocation({
-          longitude: mapRef.current.getCenter().lng,
-          latitude: mapRef.current.getCenter().lat,
-          radius: 1500.0, //meters
-          zoom: mapRef.current.getZoom(),
-        });
-
-        // Render heatmap
-
-      });
+      mapRef.current.on('moveend', handleMoveEnd);
     } else {
       const currentCenter = mapRef.current.getCenter();
-      // const currentZoom = mapRef.current.getZoom();
+      const currentZoom = mapRef.current.getZoom();
+
       if (
         currentCenter.lat !== location.latitude ||
-        currentCenter.lng !== location.longitude
+        currentCenter.lng !== location.longitude ||
+        currentZoom !== location.zoom
       ) {
+
         isProgrammaticMove.current = true;
         mapRef.current.setView(
           [location['latitude'], location['longitude']],
@@ -54,24 +75,41 @@ function MapViewer({ location, LOIResponse, setLocation, isProgrammaticMove }) {
         );
       }
     }
-  }, [location, setLocation, isProgrammaticMove]);
+
+    if (locationChangedByMoveEnd.current) {
+      console.log("Not Programmatic")
+      getLocationData().then(data => {
+        // Render heatmap; Send request to worker
+        renderHeatmap(L, data);
+      });
+      locationChangedByMoveEnd.current = false;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
 
   useEffect(() => {
 
     if (LOIResponse) {
 
-      // Get zoom from radius
+      let latLngLOIs = Object.values(LOIResponse.dbs).flat();
+      latLngLOIs = latLngLOIs.map(entry =>
+        [entry.geometry.coordinates[1], entry.geometry.coordinates[0]]
+      );
+
+      const bounds = L.latLngBounds(latLngLOIs);
 
       setLocation({
         longitude: LOIResponse.search.longitude,
         latitude: LOIResponse.search.latitude,
         radius: LOIResponse.search.radius, //meters
-        zoom: Math.ceil(14),
+        zoom: mapRef.current.getBoundsZoom(bounds)
       });
 
-      // Render heatmap
+      console.log("Programmatic")
+      renderHeatmap(L, LOIResponse);
     }
-  }, [LOIResponse, setLocation]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [LOIResponse]);
 
   return (
     <div id="map-container">
@@ -80,4 +118,4 @@ function MapViewer({ location, LOIResponse, setLocation, isProgrammaticMove }) {
   );
 }
 
-export default MapViewer;
+export default React.memo(MapViewer);
