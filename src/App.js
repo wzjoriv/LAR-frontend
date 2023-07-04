b38dc12c877f718b62302c2008c61f22c8c801b2
Josue N Rivera <josue.n.rivera@outlook.com>
2023-07-04 03:16:09 -0400

    Replace User -> Interaction

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ function App() {
     zoom: 14
   });
   const [LOIResponse, setLOIResponse] = useState(null); //loi = location of interest
-	const locationChangedByUser = useRef(false);
+  const locationChangedByInteraction = useRef(false);
   const [heatmapOn, setHeatmapOn] = useState(true);
 
   return (
     <div className="App">
-      <MapViewer location={location} locationChangedByUser={locationChangedByUser} heatmapOn={heatmapOn} setLocation={setLocation} LOIResponse={LOIResponse}/>
+      <MapViewer location={location} locationChangedByInteraction={locationChangedByInteraction} heatmapOn={heatmapOn} setLocation={setLocation} LOIResponse={LOIResponse}/>
       <div className='App-header'>
-        <SearchBar location={location} locationChangedByUser={locationChangedByUser} setHeatmapOn={setHeatmapOn} setLOIResponse={setLOIResponse} />
+        <SearchBar location={location} locationChangedByInteraction={locationChangedByInteraction} setHeatmapOn={setHeatmapOn} setLOIResponse={setLOIResponse} />
       </div>
     </div>
   );
