0f520c110c89b3042a7a53693eaa5a4a2e0afcd8
PruthviSamirPatel <pruthvipatelcollege@gmail.com>
2023-07-06 21:07:51 -0400

    Basic functionality developed. Further styling to be added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
 import MapViewer from "./Components/MapViewer";
-import { SearchBar } from './Components/SearchBar';
+import { SearchBar } from "./Components/SearchBar";
 import React, { useState, useRef } from "react";
 import buttons from "./Components/buttons.js";
+import HeatmapToggle from "./Components/HeatmapToggle";
 
 function App() {
   const [location, setLocation] = useState({
     longitude: -86.8831443,
     latitude: 40.41731893,
     radius: 4250, //meters
-    zoom: 14
+    zoom: 14,
   });
   const [LOIResponse, setLOIResponse] = useState(null); //loi = location of interest
   const locationChangedByInteraction = useRef(false);
-  const [heatmapOn, setHeatmapOn] = useState(true);
+  const [heatmapOn, setHeatmapOn] = useState(false);
+  const [buttonInfo, setButtonInfo] = useState(buttons);
 
   return (
     <div className="App">
@@ -34,6 +36,7 @@ function App() {
           locationChangedByInteraction={locationChangedByInteraction}
           setLOIResponse={setLOIResponse}
         />
+        <HeatmapToggle on={heatmapOn} setHeatmapOn={setHeatmapOn} />
       </div>
     </div>
   );
