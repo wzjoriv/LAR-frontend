11002e44faa97fb1f0cffd42e2b07054edba2530
PruthviSamirPatel <pruthvipatelcollege@gmail.com>
2023-07-10 13:49:52 -0400

    Finished both tasks for this issue

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -5,13 +5,21 @@ export default function SearchBox(props) {
   function handleChange(event) {
     props.setSearchLocation(event.target.value);
   }
+
+  function handleKeyPress(event) {
+    if (event.keyCode === 9) {
+      props.setSearchLocation(props.placeholder);
+    }
+  }
+
   return (
     <div className="searchbox">
       <input
         type="text"
         onChange={handleChange}
+        onKeyDown={handleKeyPress}
         value={props.searchLocation}
-        name="cityInput"
+        name="locationInput"
         placeholder={props.placeholder}
         title="Enter the location of interest"
       />
